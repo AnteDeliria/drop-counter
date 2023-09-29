3422b8fa6fbfd3164b2dad8193b47bdface36ba0
AnteDeliria <antedeliria@gmail.com>
2023-09-29 23:19:13 +0100

    Template detection now uses alpha channel Drops are now indexed in an easily modifiable text file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-## Add a way to exit program
+# # Add a way to exit program
+# # Output log with each observation, final counts as csv
+
 
 import subprocess
 import sys
@@ -14,20 +16,23 @@ import clipboard_monitor
 from PIL import Image, ImageGrab
 import cv2
 import numpy as np
-import json
 
 
-## Load drop table
-with open('drops.json', 'r') as file:
-    drop_table = json.load(file)
+# # Load drop table
+with open('drops.txt', 'r') as file:
+    drop_table = {}
+    for line in file:
+        line = line.strip()
+        key, values = line.split(':')
+        drop_table[key] = values.split(',')
 
 
-## Format drop selection message
+# # Format drop selection message
 select_drop_message = ''
 newline_counter = 0
 drop_table_indextokey = {}
 for i, key in enumerate(drop_table.keys()):
-    ## Assign values to indexes for drop selection
+    # # Assign values to indexes for drop selection
     drop_table_indextokey[i] = key
 
     padding = 30 - (len(str(i)) + 3 + len(key))
@@ -38,7 +43,7 @@ for i, key in enumerate(drop_table.keys()):
         select_drop_message += '\n'
 
 
-## Drop selection
+# # Drop selection
 print('\n' + select_drop_message + '\n')
 while True:
     try:
@@ -53,12 +58,12 @@ while True:
         print("\nUser interrupted the program.")
         exit(1)
 
-## Copy template names to list
+# # Copy template names to list
 template_name_list = drop_table[drop_table_indextokey[drop_table_index]]
 template_name_list
 
 
-## Target detection size selection
+# # Target detection size selection
 print('\n\nInput the width in pixels of the detected sprite as it appears on your screen. If you play on default 100% window scaling leave this blank.')
 while True:
     try:
@@ -79,33 +84,30 @@ while True:
 
 
 template_list = []
+alpha_channel_list = []
 for template_name in template_name_list:
 
-    ## Read template and resize to specified dimensions via nearest neighbor
-    base_template = cv2.imread('Templates\\' + template_name + '.png', flags = cv2.IMREAD_UNCHANGED)
+    # # Read template and resize to specified dimensions via nearest neighbor
+    base_template = cv2.imread('Templates/' + template_name + '.png', flags = cv2.IMREAD_UNCHANGED)
     if base_template is None:
         print("\nInvalid template files.")
         exit(1)
     base_template = cv2.resize(base_template, dsize=(detection_size, detection_size), interpolation = cv2.INTER_NEAREST_EXACT)
 
-    ## Split template into channels and apply the alpha value to each channel
-    blue_channel, green_channel, red_channel, alpha_channel = cv2.split(base_template)
-    blue_channel_scaled = blue_channel * (alpha_channel / 255.0)
-    green_channel_scaled = green_channel * (alpha_channel / 255.0)
-    red_channel_scaled = red_channel * (alpha_channel / 255.0)
+    # # Extract alpha channel and convert it to alpha mask for pixels where alpha = 255
+    _, _, _, alpha_channel = cv2.split(base_template)
+    alpha_channel[alpha_channel < 255] = 0
 
-    ## Merge channels and convert from float to int array
-    template = cv2.merge((blue_channel_scaled, green_channel_scaled, red_channel_scaled, alpha_channel/1))
-    template = template.astype(np.uint8)
-
-    ## Crop template to non-transparent pixels
+    # # Crop template and alpha mask to non-transparent pixels
     x, y, w, h = cv2.boundingRect(alpha_channel)
+    template = base_template[:,:,:3]
     template = template[y:y+h, x:x+w]
-    template = template[:,:,:3]
     template_list.append(template)
+    alpha_channel = alpha_channel[y:y+h, x:x+w]
+    alpha_channel_list.append(alpha_channel)
 
 
-## Keep track of template count and total count
+# # Keep track of template count and total count
 template_count = {}
 for template_name in template_name_list:
     template_count[template_name] = 0
@@ -113,13 +115,13 @@ for template_name in template_name_list:
 total_count = 0
 
 
-## Matching threshold selection
-print('\n\nInput template matching threshold. The value must be between 0 and 1.\nHigher values will increase stringency. Leave blank for default.')
+# # Matching threshold selection
+print('\n\nInput template matching threshold. The value must be between 0 and 1.\nLower values will increase stringency. Leave blank for default (0.05).')
 while True:
     try:
         user_input = input('\nInput template matching threshold: ')
         if user_input == '':
-            threshold = 0.95
+            threshold = 0.05
             break
         if 0 <= int(user_input) <= 1:
             threshold = int(user_input)
@@ -133,35 +135,36 @@ while True:
         exit(1)
 
 
-## On clipboard update
+# # On clipboard update
 def process():
 
-    ## Grab image from clipboard and convert to int array
+    # # Grab image from clipboard and convert to int array
     img = ImageGrab.grabclipboard()
     if img is None:
         return
+    img = np.array(img, dtype=np.uint8)
+
+    # # Increment kill count and convert image to int array
     global total_count
     total_count += 1
     print('Processing...')
-    img = np.array(img, dtype=np.uint8)
 
-    ## Switch channels from RGB to BGR
+    # # Switch channels from RGB to BGR
     img = img[:, :, [2, 1, 0]]
-    # print(img.shape)
-    
 
-    ## Match template and extract maximum matching value
-    ## Extract number of matches above threshold matching value
+    # # Match template and extract maximum matching value
+    # # Extract number of matches above threshold matching value
     global threshold
     for i, template in enumerate(template_list):
-        result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
-        _, maxVal, _, maxLoc = cv2.minMaxLoc(result)
-        n_matches = np.where(result >= threshold)[0].size
+        result = cv2.matchTemplate(img, template, cv2.TM_SQDIFF_NORMED, mask=alpha_channel_list[i])
+        minVal, _, _, _ = cv2.minMaxLoc(result)
+        n_matches = np.where(result <= threshold)[0].size
+        print(f'{template_name_list[i]}: {minVal}')
         if n_matches > 0:
             template_count[template_name_list[i]] += 1
-            print(f'Detected {template_name_list[i]} ({maxVal})    Total: {template_count[template_name_list[i]]}')
+            print(f'Detected {template_name_list[i]}    Total: {template_count[template_name_list[i]]}')
     
-    # Print total count
+    # # Print total count
     outmessage = f'Total kills: {total_count}    '
     for name in template_name_list:
         outmessage += f'{name}: {template_count[name]}    '
@@ -169,6 +172,7 @@ def process():
 
     print('Done')
     
+
 ## Setup clipboard monitor
 clipboard_monitor.on_update(print)
 clipboard_monitor.on_image(process)
@@ -176,22 +180,49 @@ clipboard_monitor.on_image(process)
 
 ## Run clipboard monitor
 print('\n\nPress PrintScreen to detect drops. Each press will be counted as a kill\n')
+
 clipboard_monitor.wait()
 
 
 
+# # # # Debug and unused code
 
-### Debug and unused code
 
-## Display the 10 highest values for result match
+# # # Thread clipboard monitoring
+# stop_monitoring = threading.Event()
 
+# def clipboard_monitor_thread():
+#     while not stop_monitoring.is_set():
+#         # Run clipboard monitor
+#         print('\n\nPress PrintScreen to detect drops. Each press will be counted as a kill\n')
+#         clipboard_monitor.wait()
+
+# # Start the clipboard monitoring thread
+# monitoring_thread = threading.Thread(target=clipboard_monitor_thread)
+# monitoring_thread.start()
+
+# # Listen for a key press to stop the monitoring thread
+# try:
+#     while True:
+#         key = input("\n\nPress 'q' and Enter to stop monitoring")
+#         if key.lower() == 'q':
+#             stop_monitoring.set()  # Set the flag to stop the thread
+#             monitoring_thread.join()
+#             print("STOPPED")
+#             break
+# except KeyboardInterrupt:
+#     # Handle Ctrl+C to gracefully stop the application
+#     stop_monitoring.set()
+#     monitoring_thread.join()
+
+
+# # # Display the 10 highest values for result match
 # indices = np.argpartition(result.ravel(), -10)[-10:]
 # highest_values = result.ravel()[indices]
 # highest_values
 
 
-## Match template applied on a range of scaling values
-
+# # # Match template applied on a range of scaling values
 # found = (0, 0, 1)
 # threshold = .8
 # for scale in np.linspace(1.0, 4.0, 13):
